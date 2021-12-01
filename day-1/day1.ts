3d8333ad56611a90cff7ae8fc7caa8afdde58354
Evrim <ehotamis@gmail.com>
2021-12-01 06:26:27 +0100

    part 1

--- day-1/day1.ts
+++ day-1/day1.ts
@@ -1,2 +1,9 @@
 import loadFile from "../libs/loadFile";
-const input = loadFile("./input.txt");
+import showAnswers from "../libs/showAnswer";
+const input = loadFile("./input.txt")
+  .split("\r\n")
+  .map((x) => parseInt(x));
+
+const answer1 = input.filter((x, ind) => x > input[ind - 1]).length;
+
+showAnswers([answer1]);
