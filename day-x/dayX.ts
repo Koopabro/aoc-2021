03a78773d8dfee68572f65cbe58fee66ee4730e2
Evrim <ehotamis@gmail.com>
2021-12-02 13:39:21 +0100

    preparations for tomorrow

---
+++ day-x/dayX.ts
@@ -0,0 +1,10 @@
+import loadFile from "../libs/loadFile";
+import showAnswers from "../libs/showAnswer";
+const input = loadFile("./input.txt").split("\r\n"); // Windows \r\n, Linux \n
+
+const answer1 = (input: any[]): number => {};
+const answer2 = (input: any[]): number => {};
+
+showAnswers([answer1(input), answer2(input)]);
+
+export { answer1, answer2 };
