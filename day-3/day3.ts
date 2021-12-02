03e9b8ab89c7bdce573d27c90d638d5b7dc45559
Evrim <ehotamis@gmail.com>
2021-12-02 19:22:15 +0100

    reminder

--- day-3/day3.ts
+++ day-3/day3.ts
@@ -2,6 +2,8 @@ import loadFile from "../libs/loadFile";
 import showAnswers from "../libs/showAnswer";
 const input = loadFile("./input.txt").split("\r\n"); // Windows \r\n, Linux \n
 
+//challenge of the day: use as much objects as possible
+
 const answer1 = (input: any[]): number => {};
 const answer2 = (input: any[]): number => {};
 
