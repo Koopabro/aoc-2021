d81bb72860566916ad55e0899e0d040433e54d6b
Evrim <ehotamis@gmail.com>
2021-12-03 09:22:50 +0100

    part 1

--- day-3/day3.test.ts
+++ day-3/day3.test.ts
@@ -1,8 +1,21 @@
 import { answer1, answer2 } from "./day3";
-const input = [];
+const input = [
+  "00100",
+  "11110",
+  "10110",
+  "10111",
+  "10101",
+  "01111",
+  "00111",
+  "11100",
+  "10000",
+  "11001",
+  "00010",
+  "01010",
+].map((x) => x.split("").map((x) => parseInt(x)));
 
 test("uses the first method to transverse", () => {
-  expect(answer1(input)).toBe(X);
+  expect(answer1(input)).toBe(198);
 });
 
 test("uses the second method to transverse", () => {
--- day-3/day3.ts
+++ day-3/day3.ts
@@ -1,11 +1,37 @@
 import loadFile from "../libs/loadFile";
 import showAnswers from "../libs/showAnswer";
-const input = loadFile("./input.txt").split("\r\n"); // Windows \r\n, Linux \n
+const input = loadFile("./input.txt")
+  .split("\n")
+  .map((x) => x.split("").map((x: string) => parseInt(x))); // Windows \r\n, Linux \n
 
-//challenge of the day: use as much objects as possible
+// part 1;
+const answer1 = (input: any[]): number => {
+  const verticalArrays = input[0].map((x: number, i: number) =>
+    input.map((x) => x[i])
+  );
+  const gamma = verticalArrays
+    .map((column: number[]) => {
+      return column.reduce((acc: number, cur: number) =>
+        cur === 1 ? acc + cur : acc
+      );
+    })
+    .map((x: number) => (x > input.length / 2 ? 1 : 0));
+  const epsilon = gamma.map((x: number) => Math.abs(x - 1));
+  const gammaAnswer = parseInt(gamma.join(""), 2);
+  const epsilonAnswer = parseInt(epsilon.join(""), 2);
+  return gammaAnswer * epsilonAnswer;
+};
 
-const answer1 = (input: any[]): number => {};
-const answer2 = (input: any[]): number => {};
+const answer2 = (input: any[]): number => {
+  const mostCommon = (index: number): number =>
+    input.map((x) => x[index]).reduce((acc, cur) => acc + cur) >
+    input.length / 2
+      ? 1
+      : 0;
+
+  const oxygenRating = parseInt(oxygenFilter.join(""), 2);
+  return oxygenRating;
+};
 
 showAnswers([answer1(input), answer2(input)]);
 
