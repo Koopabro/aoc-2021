d04a5fcc22d03efcdd1c10197a76e352faa2aea4
Evrim <ehotamis@gmail.com>
2021-12-07 21:52:24 +0100

    day 8 prep

---
+++ day-8/day8.test.ts
@@ -0,0 +1,10 @@
+import { answer1, answer2 } from "./day8";
+const input = ;
+
+test("Something meaningful about part 1", () => {
+  expect(answer1(input)).toBe(_);
+});
+
+test("Something meaningful about part 2", () => {
+  expect(answer2(input)).toBe(_);
+});
---
+++ day-8/day8.ts
@@ -0,0 +1,6 @@
+import loadFile from "../libs/loadFile";
+import showAnswers from "../libs/showAnswer";
+
+showAnswers([answer1(input), answer2(input)]);
+
+export { answer1, answer2 };
